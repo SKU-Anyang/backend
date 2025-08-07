ce4d43486acedcc216d76f44ad92754ea6d17257
jjun012 <duwns2057@naver.com>
2025-08-07 16:28:42 +0900

    Add main Spring Boot application class

---
+++ src/main/java/DTO/LoginRequest.java
@@ -0,0 +1,9 @@
+package DTO;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ src/test/java/com/example/An_Yang/AnYangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.An_Yang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnYangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
